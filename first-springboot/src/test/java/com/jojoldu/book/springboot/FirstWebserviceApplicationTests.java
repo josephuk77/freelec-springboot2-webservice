3ca0d427cb4e4cd583f99221c334ad5acd9cd992
josephuk77 <josephuk77@gmail.com>
2023-05-06 22:59:30 +0900

    게시판
    ㅇ

---
+++ first-springboot/src/test/java/com/jojoldu/book/springboot/FirstWebserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
